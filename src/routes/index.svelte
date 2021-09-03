6175bff8d5dcde74d3f2619d201a6e1e9d9774a3
Rudy Silva <rudysilva22@gmail.com>
2021-09-03 15:08:35 +1000

    Added Search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,33 @@
 <script>
     import { pokemon } from "../stores/pokestore";
     import PokemanCard from "../components/PokemanCard.svelte";
-    //console.log($pokemon);
+    
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $: {
+        console.log(searchTerm);
+        if(searchTerm) {
+            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        } else {
+            filteredPokemon = [...$pokemon]
+        }
+    }
+
 </script>
 <svelte:head>
     <title>Svelte Kit Pokedex</title>
 </svelte:head>
 <h1 class="text-2xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" 
+type="text" bind:value={searchTerm} placeholder="Search Pokemon">
+
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-    {#each $pokemon as pokeman}
+    {#each filteredPokemon as pokeman}
     <PokemanCard pokeman={pokeman} />
     {/each}
 </div>
-
-
 
 <style>
     h1 {
