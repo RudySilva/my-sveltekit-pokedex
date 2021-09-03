4349e4384658f70e32c675fd38fb516519d7c031
Rudy Silva <rudysilva22@gmail.com>
2021-09-03 14:30:02 +1000

    Displaying Pokemon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
     import { pokemon } from "../stores/pokestore";
-    import PokemanCard from "src/components/pokemanCard.svelte";
+    import PokemanCard from "../components/PokemanCard.svelte";
     //console.log($pokemon);
 </script>
 <svelte:head>
@@ -8,12 +8,12 @@
 </svelte:head>
 <h1 class="text-2xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each $pokemon as pokeman}
+    <PokemanCard pokeman={pokeman} />
+    {/each}
+</div>
 
-{#each $pokemon as pokeman}
-<p>
-    {pokeman.name}
-</p>
-{/each}
 
 
 <style>
